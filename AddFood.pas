fe96cbea703035d5c0021c8026e043152a74676b
Денис Давыдов <den93mos@yandex.ru>
2017-05-18 11:00:28 +0300

    Добавление формы добавления продуктов

---
+++ AddFood.pas
@@ -0,0 +1,24 @@
+unit AddFood;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TAddFoodForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AddFoodForm: TAddFoodForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
