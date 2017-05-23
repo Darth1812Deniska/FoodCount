4b61bb584afbf4674657ed2da9c4da43940004bd
Денис Давыдов <den93mos@yandex.ru>
2017-05-23 14:43:51 +0300

    Добавление главной формы

---
+++ MainUnit.pas
@@ -0,0 +1,27 @@
+unit MainUnit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TMainForm = class(TForm)
+    btnAddUser: TButton;
+    btnAddFoodGroup: TButton;
+    btnAddFood: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
